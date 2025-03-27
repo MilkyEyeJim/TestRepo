cb291d7242a8af0f80d4cbbf58e24fab7ce213a2
MilkyEyeJim <koichi_fukushima@jp.honda>
2025-03-27 22:37:42 +0900

    Update script.js

--- Code/JavaScript/script.js
+++ Code/JavaScript/script.js
@@ -2,4 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('Sample website loaded successfully!');
     console.log('HELLO');//delete this later :)
     console.log('Sample console log. Delete this');//Delete this later
+    for(var a = 0; a < 10; a++){
+        console.log("HELLO");
+    }
 });
